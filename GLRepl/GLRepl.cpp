b53fda9c4cfaee42cc6f43d96597a8284b37f161
Hunter Fernandes <h.g.fern@gmail.com>
2016-04-26 23:31:20 -0700

    Tabs -> spaces in REPL and Tests

--- GLRepl/GLRepl.cpp
+++ GLRepl/GLRepl.cpp
@@ -18,86 +18,86 @@ GraphLang::Parser::OneOfRType orderedParse(GraphLang::Parser& p);
 
 bool doForString(string input)
 {
-	if (input == ":q")
-	{
-		return false;
-	}
+    if (input == ":q")
+    {
+        return false;
+    }
 
-	GraphLang::Tokenizer::String s1, s2;
-	istringstream iss(input);
-	GraphLang::Parser parser(iss);
+    GraphLang::Tokenizer::String s1, s2;
+    istringstream iss(input);
+    GraphLang::Parser parser(iss);
 
-	auto got = orderedParse(parser);
-	auto res = get<1>(got);
+    auto got = orderedParse(parser);
+    auto res = get<1>(got);
 
-	if (!get<0>(res))
-	{
-		cout << "Didn't successfully parse any: " << get<1>(res) << endl;
-		return true;
-	}
+    if (!get<0>(res))
+    {
+        cout << "Didn't successfully parse any: " << get<1>(res) << endl;
+        return true;
+    }
 
-	auto tok = std::move(get<0>(got));
+    auto tok = std::move(get<0>(got));
 
-	if (parser.eof() || parser.matchLiteral(";"))
-	{
-		cout << "Got a " << tok->name() << endl;
-		cout << "Yielding Exec Actions..." << endl;
+    if (parser.eof() || parser.matchLiteral(";"))
+    {
+        cout << "Got a " << tok->name() << endl;
+        cout << "Yielding Exec Actions..." << endl;
 
-		Action::AttachAttributeYielder attrYielder;
-		attrYielder(*tok.get(), Tokenizer::TokenVisitor::Root);
-		cout << "Will attach attributes [" << attrYielder.getActions().size() << "]" << endl;
-		for (auto& action: attrYielder.getActions())
-		{
-			auto aaptr = dynamic_cast<Action::AttachAttribute*>(action.get());
-			cout << aaptr->node_name()
-				<< "\t: `" << aaptr->property_name()
-				<< "`\t=\t" << aaptr->value().getType() << endl;
-		}
+        Action::AttachAttributeYielder attrYielder;
+        attrYielder(*tok.get(), Tokenizer::TokenVisitor::Root);
+        cout << "Will attach attributes [" << attrYielder.getActions().size() << "]" << endl;
+        for (auto& action: attrYielder.getActions())
+        {
+            auto aaptr = dynamic_cast<Action::AttachAttribute*>(action.get());
+            cout << aaptr->node_name()
+                << "\t: `" << aaptr->property_name()
+                << "`\t=\t" << aaptr->value().getType() << endl;
+        }
 
-		cout << "\n\n\n";
+        cout << "\n\n\n";
 
-		Action::AttachRelationYielder relYielder;
-		relYielder(*tok.get(), Tokenizer::TokenVisitor::Root);
-		cout << "Building the following relations: [" << relYielder.getActions().size() << "]\n";
-		for (auto& action : relYielder.getActions())
-		{
-			auto arptr = dynamic_cast<Action::AttachRelation*>(action.get());
-			cout << arptr->fromNode() << " \t-" << arptr->relName()
-				<< "-> \t" << arptr->toNode() << endl;
-		}
+        Action::AttachRelationYielder relYielder;
+        relYielder(*tok.get(), Tokenizer::TokenVisitor::Root);
+        cout << "Building the following relations: [" << relYielder.getActions().size() << "]\n";
+        for (auto& action : relYielder.getActions())
+        {
+            auto arptr = dynamic_cast<Action::AttachRelation*>(action.get());
+            cout << arptr->fromNode() << " \t-" << arptr->relName()
+                << "-> \t" << arptr->toNode() << endl;
+        }
 
-	}
-	else
-	{
-		cout << "First consumable was " << tok->name()
-			<< ", but characters were left unread!!!";
-	}
+    }
+    else
+    {
+        cout << "First consumable was " << tok->name()
+            << ", but characters were left unread!!!";
+    }
 
-	return true;
+    return true;
 }
 
 
 int main()
 {
-	bool quit = false;
-	while (!quit)
-	{
-		string line;
-		cout << "\n\n$$$ ";
-		getline(cin, line);
-		quit = !doForString(line);
-	}
+    bool quit = false;
+    while (!quit)
+    {
+        string line;
+        cout << "\n\n$$$ ";
+        getline(cin, line);
+        quit = !doForString(line);
+    }
 
-	cout << "Hasta la vista, baby. :)" << endl;
+    cout << "Hasta la vista, baby. :)" << endl;
 }
 
 // The godless function that abuses macros.
 GraphLang::Parser::OneOfRType orderedParse(GraphLang::Parser& p)
 {
-#define GRL_TOKEN_ENTRY(PRI,TOKT,TOKNAME)		::GraphLang::Tokenizer::##TOKNAME,
+#define GRL_TOKEN_ENTRY(PRI,TOKT,TOKNAME)        ::GraphLang::Tokenizer::##TOKNAME,
 #define GRL_TOKEN_ENTRY_LAST(PRI,TOKT,TOKNAME)  ::GraphLang::Tokenizer::##TOKNAME
 
-	return p.oneOf<
-#include "../GraphLang/parsepriority.h"	
-	>();
+    return p.oneOf<
+#include "../GraphLang/parsepriority.h"    
+    >();
 }
--- ParseTests/stringtest.cpp
+++ ParseTests/stringtest.cpp
@@ -4,56 +4,56 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ParseTests
-{		
-	TEST_CLASS(StringTest)
-	{
-	public:
-		TEST_METHOD(NotStrings)
-		{
-			assertNotParses("");
-			assertNotParses("1.023");
-			assertNotParses("()");
-			assertNotParses("test_identifier");
-		}
+{        
+    TEST_CLASS(StringTest)
+    {
+    public:
+        TEST_METHOD(NotStrings)
+        {
+            assertNotParses("");
+            assertNotParses("1.023");
+            assertNotParses("()");
+            assertNotParses("test_identifier");
+        }
 
-		TEST_METHOD(TestNormals)
-		{
-			assertParsesTo("\"\"", "");
-			assertParsesTo("\"a\"", "a");
-			assertParsesTo("\"A\"", "A");
-			assertParsesTo("\"Hunter's\"", "Hunter's");
-		}
+        TEST_METHOD(TestNormals)
+        {
+            assertParsesTo("\"\"", "");
+            assertParsesTo("\"a\"", "a");
+            assertParsesTo("\"A\"", "A");
+            assertParsesTo("\"Hunter's\"", "Hunter's");
+        }
 
-		TEST_METHOD(TestEscapes)
-		{
-			assertParsesTo("\"A\\\\Backslash\"", "A\\Backslash");
-			assertParsesTo("\"A\\nNewline\"", "A\nNewline");
-			assertParsesTo("\"A\\\"Quote\"", "A\"Quote");
-		}
-	private:
-		void assertParsesTo(std::string source, std::string becomes)
-		{
-			std::istringstream iss(source);
-			GraphLang::Parser parser(iss);
-			GraphLang::Tokenizer::String str;
+        TEST_METHOD(TestEscapes)
+        {
+            assertParsesTo("\"A\\\\Backslash\"", "A\\Backslash");
+            assertParsesTo("\"A\\nNewline\"", "A\nNewline");
+            assertParsesTo("\"A\\\"Quote\"", "A\"Quote");
+        }
+    private:
+        void assertParsesTo(std::string source, std::string becomes)
+        {
+            std::istringstream iss(source);
+            GraphLang::Parser parser(iss);
+            GraphLang::Tokenizer::String str;
 
-			auto rr = parser.require(str);
+            auto rr = parser.require(str);
 
-			auto errMessage = std::move(std::get<1>(rr));
-			Assert::IsTrue(std::get<0>(rr), std::wstring(errMessage.begin(), errMessage.end()).c_str());
-			Assert::AreEqual(str.val, becomes);
-		}
+            auto errMessage = std::move(std::get<1>(rr));
+            Assert::IsTrue(std::get<0>(rr), std::wstring(errMessage.begin(), errMessage.end()).c_str());
+            Assert::AreEqual(str.val, becomes);
+        }
 
-		void assertNotParses(std::string source)
-		{
-			std::istringstream iss(source);
-			GraphLang::Parser parser(iss);
-			GraphLang::Tokenizer::String str;
+        void assertNotParses(std::string source)
+        {
+            std::istringstream iss(source);
+            GraphLang::Parser parser(iss);
+            GraphLang::Tokenizer::String str;
 
-			auto rr = parser.require(str);
+            auto rr = parser.require(str);
 
-			auto errMessage = std::move(std::get<1>(rr));
-			Assert::IsFalse(std::get<0>(rr), std::wstring(str.val.begin(), str.val.end()).c_str());
-		}
-	};
+            auto errMessage = std::move(std::get<1>(rr));
+            Assert::IsFalse(std::get<0>(rr), std::wstring(str.val.begin(), str.val.end()).c_str());
+        }
+    };
 }
